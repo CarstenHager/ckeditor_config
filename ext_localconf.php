d4f37afed86340f7d1d48b704fe37b2cbb742fff
p208589 <p208589@localdomain>
2017-03-19 10:40:33 +0100

    Eigene ConfigDatei in der ext_localconf registriert

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,8 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+/***************
+ * Add default RTE configuration for bootstrap package
+ */
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['bootstrap'] = 'EXT:ckeditor_config/Configuration/PageTS/RTE/Default.yaml';
